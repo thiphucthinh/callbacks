0bf3600fbb42081c967bc503a08a1d340e7d59ad
Toddric <toddric29@gmail.com>
2023-08-14 20:34:54 -0700

    second answer

--- problems/02-my-map.js
+++ problems/02-my-map.js
@@ -18,8 +18,20 @@ console.log(result2);   // [ 'RUN!', 'FORREST!' ]
 
 function myMap(array, cb) {
     // Your code here
+    let newArray = [];
+    for(let el of array) {
+        newArray.push(cb(el));
+    }
+    return newArray;
 }
 
+let result1 = myMap([100, 25, 81, 64], Math.sqrt);
+console.log(result1);   // [ 10, 5, 9, 8 ]
+
+let result2 = myMap(['run', 'Forrest'], function (el) {
+    return el.toUpperCase() + '!';
+});
+console.log(result2);   // [ 'RUN!', 'FORREST!' ]
 /*****************DO NOT MODIFY ANYTHING UNDER THIS  LINE**********************/
 try {
     module.exports = myMap;
