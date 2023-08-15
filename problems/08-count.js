edd65aaf280b8afde87ade60ec62137d0f2299ef
Toddric <toddric29@gmail.com>
2023-08-14 21:42:52 -0700

    eight answer

--- problems/08-count.js
+++ problems/08-count.js
@@ -27,9 +27,34 @@ console.log(result4); // 0
 *******************************************************************************/
 
 function count(array, cb) {
-  // Your code here
+  let count = 0;
+  for (let el of array) {
+    if (cb(el)) {
+      count++;
+    }
+  }
+  return count;
 }
 
+let result1 = count([18, 5, 32, 7, 100], function (n) {
+  return n % 2 === 0;
+});
+console.log(result1); // 3
+
+let result2 = count([17, 5, 31, 7, 100], function (n) {
+  return n % 2 === 0;
+});
+console.log(result2); // 1
+
+let result3 = count(['follow', 'the', 'yellow', 'brick', 'road'], function (str) {
+  return str.includes('o');
+});
+console.log(result3); // 3
+
+let result4 = count(['follow', 'the', 'yellow', 'brick', 'road'], function (str) {
+  return str.includes('x');
+});
+console.log(result4); // 0
 /*****************DO NOT MODIFY ANYTHING UNDER THIS  LINE**********************/
 try {
   module.exports = count;
