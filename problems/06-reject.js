60b2fb84f697314ed21c5204d4fd2ad8bb65bf57
Toddric <toddric29@gmail.com>
2023-08-14 21:27:57 -0700

    sixth answer

--- problems/06-reject.js
+++ problems/06-reject.js
@@ -22,9 +22,24 @@ console.log(reject(['breadth', 'GRAPH', 'depth', 'height'], hasA)); // [ 'depth'
 *******************************************************************************/
 
 function reject(array, cb) {
-  // Your code here
+  let newArray = [];
+  for (let el of array) {
+    if (!cb(el)) {
+      newArray.push(el);
+    }
+  }
+  return newArray;
 }
 
+let isEven = function(n) {
+  return n % 2 === 0;
+};
+console.log(reject([7, 8, 5, 6, 12, 11], isEven)); // [ 7, 5, 11 ]
+
+let hasA = function(s) {
+  return s.toLowerCase().includes('a');
+};
+console.log(reject(['breadth', 'GRAPH', 'depth', 'height'], hasA)); // [ 'depth', 'height' ]
 /*****************DO NOT MODIFY ANYTHING UNDER THIS  LINE**********************/
 try {
   module.exports = reject;
