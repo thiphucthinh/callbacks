951f898f15b271a6662ef42cf1d9f30138396b15
Toddric <toddric29@gmail.com>
2023-08-14 21:08:32 -0700

    fourth answer

--- problems/04-my-filter.js
+++ problems/04-my-filter.js
@@ -21,8 +21,24 @@ console.log(result2);      // ['choose', 'words', 'only']
 
 function myFilter(array, cb) {
     // Your code here
+    let newArray = []
+    for (let el of array) {
+        if (cb(el)) {
+            newArray.push(el)
+        }
+    }
+    return newArray
 }
 
+let result1 = myFilter([5, 7, 4, 3, 8], function (n) {
+    return n % 2 === 0;
+});
+console.log(result1);       // [ 4, 8 ]
+
+let result2 = myFilter(['choose', 'big', 'words', 'only'], function (s) {
+    return s.length > 3;
+});
+console.log(result2);      // ['choose', 'words', 'only']
 /*****************DO NOT MODIFY ANYTHING UNDER THIS  LINE**********************/
 try {
     module.exports = myFilter;
