7414e5bdbd51a71cbe6ced53fa85f9b68c50c745
Toddric <toddric29@gmail.com>
2023-08-15 21:25:29 -0700

    21st answer

--- problems/21-at-most.js
+++ problems/21-at-most.js
@@ -21,9 +21,28 @@ console.log(atMost(['boat', 'arc', 'cat', 'car', 'academy'], 1, startsWithA));  
 *******************************************************************************/
 
 function atMost(array, max, cb) {
-  // Your code here
+  let count = 0;
+  for (let el of array) {
+    if (cb(el)) {
+      count++;
+    }
+  }
+  return count <= max;
 }
 
+let isPositive = function (n) {
+  return n > 0;
+};
+let startsWithA = function (s) {
+  return s[0].toUpperCase() === 'A';
+};
+
+console.log(atMost([6, -2, 4, -1], 3, isPositive));                             // true
+console.log(atMost([6, -2, 4, 1], 3, isPositive));                              // true
+console.log(atMost([6, 2, 4, 1], 3, isPositive));                               // false
+console.log(atMost(['boat', 'cat', 'car'], 1, startsWithA));                    // true
+console.log(atMost(['boat', 'cat', 'car', 'academy'], 1, startsWithA));         // true
+console.log(atMost(['boat', 'arc', 'cat', 'car', 'academy'], 1, startsWithA));  // false
 /*****************DO NOT MODIFY ANYTHING UNDER THIS  LINE**********************/
 try {
   module.exports = atMost;
