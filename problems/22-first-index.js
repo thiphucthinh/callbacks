70727565d58d04ca1140df84d899c9f9a3c8bdf7
Thi Phuc Thinh Huynh <ph.thinh905@gmail.com>
2023-08-15 21:31:31 -0700

    problem 22 solution

--- problems/22-first-index.js
+++ problems/22-first-index.js
@@ -23,8 +23,29 @@ console.log(result3); // -1
 *******************************************************************************/
 
 function firstIndex(array, cb) {
-  // Your code here
+  for (let i = 0; i < array.length; i++) {
+    if (cb(array[i])) {
+      return i;
+    }
+  }
+  return -1;
 }
+
+// Example
+let result1 = firstIndex([3, 7, 8, 10], function (n) {
+  return n % 2 === 0;
+});
+console.log(result1); // 2
+
+let result2 = firstIndex(['dog', 'cat', 'tree'], function (s) {
+  return s.length === 3;
+});
+console.log(result2); // 0
+
+let result3 = firstIndex(['canine', 'feline', 'tree'], function (s) {
+  return s.length === 3;
+});
+console.log(result3); // -1
 
 /*****************DO NOT MODIFY ANYTHING UNDER THIS  LINE**********************/
 try {
